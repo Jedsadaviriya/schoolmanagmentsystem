6613f630b4e47b9a62e8b308918f3cdc864cbc76
Setthawut Jedsadaviriya <setthawut.j2007@gmail.com>
2025-04-23 13:24:22 +0200

    removed server .next
    removed the dumb move of putting it on the domain

--- .next/static/chunks/src_app_layout_jsx_c0237562._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__31a1a670._.css",
-  "static/chunks/_b08659a5._.js"
-],
-    source: "dynamic"
-});
