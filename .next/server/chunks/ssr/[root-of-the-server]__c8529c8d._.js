201b077e2d681ce39c6414eaab5603701283c141
sleep404 <loris.stahlberg@icloud.com>
2025-04-25 13:42:57 +0200

    Schönerer header + animation bei buttons

--- .next/server/chunks/ssr/[root-of-the-server]__c8529c8d._.js
+++ .next/server/chunks/ssr/[root-of-the-server]__c8529c8d._.js
@@ -160,7 +160,7 @@ async function Modules() {
                                     }, this),
                                     /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])(__TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$client$2f$app$2d$dir$2f$link$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["default"], {
                                         href: `/module/${module._id}`,
-                                        className: __TURBOPACK__imported__module__$5b$project$5d2f$src$2f$app$2f$module$2f$page$2e$module$2e$css__$5b$app$2d$rsc$5d$__$28$css__module$29$__["default"].viewButton,
+                                        className: `${__TURBOPACK__imported__module__$5b$project$5d2f$src$2f$app$2f$module$2f$page$2e$module$2e$css__$5b$app$2d$rsc$5d$__$28$css__module$29$__["default"].viewButton} animated-button-secondary`,
                                         children: "Details anzeigen"
                                     }, void 0, false, {
                                         fileName: "[project]/src/app/module/page.jsx",
