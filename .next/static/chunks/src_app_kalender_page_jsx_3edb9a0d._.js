6ab02a95c13848d2609ec2041c9f4091345780d1
sleep404 <loris.stahlberg@icloud.com>
2025-05-09 09:18:26 +0200

    notifcation component

---
+++ .next/static/chunks/src_app_kalender_page_jsx_3edb9a0d._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_13ecbbbd._.css",
+  "static/chunks/src_294d6b58._.js"
+],
+    source: "dynamic"
+});
