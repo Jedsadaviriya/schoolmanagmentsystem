8f711e91a2115c896d2a5296f145c7200d827b2d
sleep404 <loris.stahlberg@icloud.com>
2025-07-20 17:48:25 +0200

    deleted a few files for proper merge

--- .next/static/chunks/src_app_kalender_page_jsx_3edb9a0d._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_13ecbbbd._.css",
-  "static/chunks/src_294d6b58._.js"
-],
-    source: "dynamic"
-});
