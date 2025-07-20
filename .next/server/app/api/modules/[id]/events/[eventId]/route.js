4ea41c4bd22bf40946d331128627b4510c631863
sleep404 <loris.stahlberg@icloud.com>
2025-07-20 16:20:34 +0200

    added grades-demo and modules-demo

---
+++ .next/server/app/api/modules/[id]/events/[eventId]/route.js
@@ -0,0 +1,7 @@
+const CHUNK_PUBLIC_PATH = "server/app/api/modules/[id]/events/[eventId]/route.js";
+const runtime = require("../../../../../../chunks/[turbopack]_runtime.js");
+runtime.loadChunk("server/chunks/node_modules_next_f138e4de._.js");
+runtime.loadChunk("server/chunks/[root-of-the-server]__929634ef._.js");
+runtime.getOrInstantiateRuntimeModule("[project]/.next-internal/server/app/api/modules/[id]/events/[eventId]/route/actions.js [app-rsc] (server actions loader, ecmascript)", CHUNK_PUBLIC_PATH);
+runtime.getOrInstantiateRuntimeModule("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/api/modules/[id]/events/[eventId]/route.js [app-route] (ecmascript)\" } [app-route] (ecmascript)", CHUNK_PUBLIC_PATH);
+module.exports = runtime.getOrInstantiateRuntimeModule("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/api/modules/[id]/events/[eventId]/route.js [app-route] (ecmascript)\" } [app-route] (ecmascript)", CHUNK_PUBLIC_PATH).exports;
